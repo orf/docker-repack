2d0e95f24ce3610104a5f66fdb87b259424d32a1
Tom Forbes <tom@tomforb.es>
2024-09-04 19:33:37 +0100

    max build space

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::io::{stderr, IsTerminal};
+use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
 use byte_unit::{AdjustedByte, Byte, UnitType};
 use rayon::iter::{FromParallelIterator, IndexedParallelIterator, ParallelIterator};
 use tracing::{info_span, Span};
@@ -25,7 +27,6 @@ const PBAR_TEMPLATE: &str = "{span_child_prefix} {msg} {percent}% {wide_bar} {pe
 
 const SPINNER_TEMPLATE: &str = "{span_child_prefix} {spinner} {msg} {per_sec}";
 
-#[inline(always)]
 fn setup_span_bar(span: &Span, size: usize, message: &'static str) -> Span {
     span.pb_set_message(message);
     span.pb_set_style(&indicatif::ProgressStyle::default_bar().template(PBAR_TEMPLATE).unwrap());
@@ -33,7 +34,6 @@ fn setup_span_bar(span: &Span, size: usize, message: &'static str) -> Span {
     Span::current()
 }
 
-#[inline(always)]
 fn setup_span_spinner(span: &Span, message: &'static str) -> Span {
     span.pb_set_message(message);
     span.pb_set_style(
@@ -45,6 +45,15 @@ fn setup_span_spinner(span: &Span, message: &'static str) -> Span {
 }
 
 #[inline(always)]
+fn tick(span: &Span, total: usize, is_term: bool) -> usize {
+    if is_term {
+        span.pb_inc(1);
+    } else {
+        span.pb_inc(1);
+    }
+    total + 1
+}
+
 pub fn progress_parallel_collect<V: FromParallelIterator<T>, T: Send>(
     message: &'static str,
     iterator: impl IndexedParallelIterator<Item = anyhow::Result<T>>,
@@ -52,15 +61,18 @@ pub fn progress_parallel_collect<V: FromParallelIterator<T>, T: Send>(
     let span = info_span!("task");
     let entered = span.enter();
     let span = setup_span_bar(&span, iterator.len(), message);
+    let is_term = stderr().is_terminal();
+    let total_counter = AtomicUsize::new(0);
+
     iterator
         .inspect(move |_| {
-            span.pb_inc(1);
+            let total = total_counter.fetch_add(1, Ordering::Relaxed);
+            tick(&span, total, is_term);
             let _ = entered;
         })
         .collect::<anyhow::Result<V>>()
 }
 
-#[inline(always)]
 pub fn progress_iter<T>(
     message: &'static str,
     iterator: impl ExactSizeIterator<Item = T>,
@@ -69,19 +81,23 @@ pub fn progress_iter<T>(
     let span = info_span!("task");
     let entered = span.enter();
     let span = setup_span_bar(&span, size, message);
+    let is_term = stderr().is_terminal();
+    let mut total = 0;
     iterator.inspect(move |_| {
-        span.pb_inc(1);
+        total = tick(&span, total, is_term);
         let _ = entered;
     })
 }
 
-#[inline(always)]
 pub fn spinner_iter<T>(message: &'static str, iterator: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
     let span = info_span!("task");
     let entered = span.enter();
     let span = setup_span_spinner(&span, message);
+    let is_term = stderr().is_terminal();
+    let mut total = 0;
+
     iterator.inspect(move |_| {
-        span.pb_inc(1);
+        total = tick(&span, total, is_term);
         let _ = entered;
     })
 }
