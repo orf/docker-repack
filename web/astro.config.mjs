5c3a64ffbb076c981c30cf64973905b8726b4210
Tom Forbes <tom@tomforb.es>
2024-09-13 00:18:17 +0100

    Fix build

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.SITE_URL || "http://localhost",
+  base: process.env.SITE_PREFIX || '/docs',
   integrations: [tailwind(), react()],
   vite: {
     plugins: [ViteYaml()],
--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_PREFIX = process.env.SITE_PREFIX || "";
+export const SITE_PREFIX = import.meta.env.BASE_URL;
 export const SITE_NAME = "Docker Repack";
 export const SITE_LOGO = "https://flowbite.com/docs/images/logo.svg";
