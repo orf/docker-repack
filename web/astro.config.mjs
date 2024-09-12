8ed029a2d53d3b1765db33153db244838a15969a
Tom Forbes <tom@tomforb.es>
2024-09-13 00:38:47 +0100

    Fix urls

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_ORIGIN,
   base: process.env.SITE_PREFIX,
